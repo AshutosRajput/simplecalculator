33e8b3a0cc08121c37c129c82fc2a88644a0dd0f
AshutosRajput <rajashutosh491@gmail.com>
2022-09-22 21:37:20 +0530

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Container(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.bottomRight,
+                child: Text(
+
+                  text.toString(),
+                  style: TextStyle(
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.deepOrange),
+                ),
+
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                customOutlineButton("9"),
+                customOutlineButton("8"),
+                customOutlineButton("7"),
+                customOutlineButton("+"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                customOutlineButton("6"),
+                customOutlineButton("5"),
+                customOutlineButton("4"),
+                customOutlineButton("-"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                customOutlineButton("3"),
+                customOutlineButton("2"),
+                customOutlineButton("1"),
+                customOutlineButton("x"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                customOutlineButton("C"),
+                customOutlineButton("0"),
+                customOutlineButton("="),
+                customOutlineButton("/"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customOutlineButton(String val) {
+
+    final String text = val; // initialized the paramater -  because dart
+    return Expanded(
+      child: OutlinedButton(
+
+
+
+
+        onPressed: () => btnClicked(text),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 35.0, color: Colors.black),
+        ),
+
+      ),
+    );
+  }
+
+  int first = 0, second =0; // int cannot be null given the value is to be used in the code
+  String? res, text; // null safety added
+  String? opp; //null safety added.
+  void btnClicked(String btnText) {
+    if (btnText == "C") {
+      res = "";
+      text = "";
+      first = 0;
+      second = 0;
+    } else if (btnText == "+" ||
+        btnText == "-" ||
+        btnText == "x" ||
+        btnText == "/") {
+      first = int.parse(text.toString());
+      res = "";
+      opp = btnText;
+    } else if (btnText == "=") {
+      second = int.parse(text.toString());
+      if (opp == "+") {
+        res = (first + second).toString();
+      }
+      if (opp == "-") {
+        res = (first - second).toString();
+      }
+      if (opp == "x") {
+        res = (first * second).toString();
+      }
+      if (opp == "/") {
+        res = (first ~/ second).toString();
+      }
+    } else {
+      res = int.parse("$text$btnText").toString(); // concatination to be done as in parse section
+    }
+    setState(() {
+      text = res;
+    });
+  }
+}
+
+OutlineButton({required EdgeInsets padding, required void Function() onPressed, required Text child}) {
+}
